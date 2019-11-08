015439fcef3d8566082193612f6a77b43e5085b5
ikemura23 <banbara.work@gmail.com>
2019-11-08 20:44:27 +0900

    CameraPreviewをMaterialAppでラップした

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,20 @@ List<CameraDescription> cameras;
 
 Future<void> main() async {
   cameras = await availableCameras();
-  runApp(CameraApp());
+  runApp(MainApp());
 }
 
+// Main
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraApp(),
+    );
+  }
+}
+
+// カメラ
 class CameraApp extends StatefulWidget {
   @override
   _CameraAppState createState() => _CameraAppState();
@@ -40,9 +51,13 @@ class _CameraAppState extends State<CameraApp> {
     if (!controller.value.isInitialized) {
       return Container();
     }
-    return AspectRatio(
-        aspectRatio:
-        controller.value.aspectRatio,
-        child: CameraPreview(controller));
+    return MaterialApp(
+      home: Scaffold(
+        body: AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: CameraPreview(controller),
+        ),
+      ),
+    );
   }
 }
