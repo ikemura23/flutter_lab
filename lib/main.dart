23777fbbfaa66835d6e7fec706fe18d1f009398c
ikemura23 <banbara.work@gmail.com>
2019-11-09 23:07:11 +0900

    カメラ撮影のみを実装、撮影→保存が成功

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
 
 List<CameraDescription> cameras;
 
@@ -27,11 +29,19 @@ class CameraApp extends StatefulWidget {
 
 class _CameraAppState extends State<CameraApp> {
   CameraController controller;
+  String imagePath;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     super.initState();
-    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    // カメラ初期設定
+    controller = CameraController(
+      cameras[0], //背面カメラのみ
+      ResolutionPreset.medium,
+      enableAudio: false, //音声は不要
+    );
     controller.initialize().then((_) {
       if (!mounted) {
         return;
@@ -53,11 +63,63 @@ class _CameraAppState extends State<CameraApp> {
     }
     return MaterialApp(
       home: Scaffold(
-        body: AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: CameraPreview(controller),
+        key: _scaffoldKey,
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: CameraPreview(controller),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                icon: Icon(Icons.camera_alt),
+                onPressed: () => onTakePictureButtonPressed(),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+
+  /// カメラボタン押下
+  void onTakePictureButtonPressed() {
+    takePicture().then((String filePath) {
+      setState(() {
+        imagePath = filePath;
+      });
+      if (filePath != null) showInSnackBar('Picture saved to $filePath');
+    });
+  }
+
+  void showInSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
+  /// カメラ撮影処理
+  Future<String> takePicture() async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String dirPath = '${directory.path}/Pictures/flutter';
+    await Directory(dirPath).create(recursive: true);
+    final String filePath = '$dirPath/${timestamp()}.jpg';
+
+    try {
+      controller.takePicture(filePath);
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+    return filePath;
+  }
+
+  void _showCameraException(CameraException e) {
+    logError(e.code, e.description);
+  }
+
+  void logError(String code, String message) =>
+      print('Error: $code/nError Message: $message');
 }
